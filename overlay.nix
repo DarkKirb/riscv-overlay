c475b582c6073fb7514a75ff5da172eecfd8180b
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-07-11 07:57:04 +0200

    enable python overlay

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,7 @@
 with (import ./lib.nix);
   composeManyExtensions [
     (import ./overlays/ghc.nix)
+    (import ./overlays/python.nix)
     (import ./overlays/require-native-build.nix)
     (import ./overlays/test-timeouts.nix)
   ]
