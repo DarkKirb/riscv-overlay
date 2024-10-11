47b3b0e989ff06046004be0c76d24af9a46a0c29
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-10-11 20:46:21 +0200

    use patched nixpkgs for overlay

--- overlays/haskell.nix
+++ overlays/haskell.nix
@@ -1,8 +1,10 @@
 self: super: haskellSelf: haskellSuper: {
   # Disable library profiling because it doesn’t work lol
-  mkDerivation = args: haskellSuper.mkDerivation ({
-    enableLibraryProfiling = false;
-  } // args);
+  mkDerivation = args:
+    haskellSuper.mkDerivation ({
+        enableLibraryProfiling = false;
+      }
+      // args);
   # Very expensive testsuite that only runs natively
   happy = self.haskell.lib.dontCheck haskellSuper.happy;
 }
--- release-small.nix
+++ release-small.nix
@@ -1,1 +1,30 @@
-{ nixpkgs }: import "${nixpkgs}/nixos/release-small.nix" { supportedSystems = ["riscv64-linux"]; }
+{nixpkgs}: let
+  pkgs = import <nixpkgs> {
+    system = "x86_64-linux";
+  };
+
+  nixpkgs' = with pkgs;
+    stdenv.mkDerivation {
+      name = "nixpkgs-patched";
+      src = pkgs.path;
+      dontUnpack = true;
+      buildPhase = "";
+      installPhase = ''
+        cp -r $src $out
+        chmod +w -R $out/
+        mv $out/default.nix $out/default2.nix
+        cp ${writeText "default.nix" ''
+          {overlays ? [], ...} @ args: import ./default2.nix (args // {overlays = overlays ++ [(import ${./.}/default.nix)]; })
+        ''} $out/default.nix
+      '';
+    };
+in
+  with pkgs.lib;
+    import "${nixpkgs}/nixos/release-small.nix" {
+      supportedSystems = ["riscv64-linux"];
+      nixpkgs = {
+        outPath = cleanSource nixpkgs';
+        revCount = 56789;
+        shortRev = "gfedcba";
+      };
+    }
