7e85f4312717f41b4e9ea55f433813fe28beb9a9
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-27 18:53:10 +0100

    dbus-python: disable tests

--- overlays/python/broken-tests.nix
+++ overlays/python/broken-tests.nix
@@ -1,15 +1,18 @@
 self: super: pythonSelf: pythonSuper: {
-  hypothesis = pythonSuper.hypothesis.overrideAttrs {
+  cbor2 = pythonSuper.cbor2.overrideAttrs {
     doInstallCheck = false;
   };
-  psutil = pythonSuper.psutil.overrideAttrs (super: {
-    requiredSystemFeatures = super.requiredSystemFeatures or [ ] ++ [ "native-riscv" ];
-  });
-  cbor2 = pythonSuper.cbor2.overrideAttrs {
+  dbus-python = pythonSuper.dbus-python.overrideAttrs {
+    doInstallCheck = false;
+  };
+  hypothesis = pythonSuper.hypothesis.overrideAttrs {
     doInstallCheck = false;
   };
   mypy = pythonSuper.mypy.overrideAttrs {
     env = { };
     doInstallCheck = false;
   };
+  psutil = pythonSuper.psutil.overrideAttrs (super: {
+    requiredSystemFeatures = super.requiredSystemFeatures or [ ] ++ [ "native-riscv" ];
+  });
 }
